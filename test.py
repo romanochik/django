15e6d31b1e2c9e8ffadf2dad8587118bad109947
Your Name <you@example.com>
2022-01-18 16:57:57 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+'''
+pip install django
+django-admin startproject testmanager - создание проекта с названием taskmanager
+pythom manage.py runserver
+
+выгрузть файлы в гид
+1. скачать git на компьютер https://git-scm.com/
+2.  git init
+    git add .
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/romanochik/django.git
+    git push -u origin main
+3. Диспечер учётных данных - удалить чужую учётку
+
+'''
