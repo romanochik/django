346f58678864ab99f98cac00d6bbdd17f5dfc453
Your Name <you@example.com>
2022-01-25 17:34:47 +0500

    first commit

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('news', views.news),
+    path('bebrik', views.bebrik),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+# Creaet your views here
+#from django.http import HttpRespsonse
+
 
 def index(request):
     return render(request, 'main/index.html')
+
+def news(request):
+    return render(request, 'main/news.html')
+
+def bebrik(request):
+    return render(request, 'main/bebrik.html')
