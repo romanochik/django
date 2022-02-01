b2cab04a64bf787b9ebabacb94ed324d27060ab0
Your Name <you@example.com>
2022-02-01 17:34:37 +0500

    first commit

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xi=8(q32=+8lqvk^n23l!k47&yi%izh0@g#(2l^hoi0@oogzqx'
+SECRET_KEY = 'django-insecure-+qqx0q@sj-^#%vk*4r7i@in*%75iu4o)9ul6diwgdj0$%d_lyx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
